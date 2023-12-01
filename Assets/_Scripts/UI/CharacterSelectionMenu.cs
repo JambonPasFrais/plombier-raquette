aeaecc0dfbe451926058468663f63c82eb0ed459
Remy <remy.jabbo@gmail.com>
2023-12-01 11:39:47 +0100

    Adaptative Character Selection Menu
    Character display right number of "showrooms" depending on game mode selected (simple or double)
    Reset menu when go back to previous menus

--- Assets/_Scripts/UI/CharacterSelectionMenu.cs
+++ Assets/_Scripts/UI/CharacterSelectionMenu.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
@@ -9,31 +10,43 @@ using UnityEngine.UI;
 
 public class CharacterSelectionMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject _characterUIPrefab;
+	[SerializeField] private GameObject _characterUIPrefab;
 	[SerializeField] private List<CharacterData> _characters = new List<CharacterData>();
 	[SerializeField] private Transform _charactersListTransform;
-	[SerializeField] private List<TextMeshProUGUI> _selectedCharactersName = new List<TextMeshProUGUI>();
-	[SerializeField] private List<Transform> _characterModelLocation = new List<Transform>();
-	[SerializeField] private List<Image> _selectedCharacterBackground = new List<Image>();
+	private List<TextMeshProUGUI> _currentSelectedCharactersName = new List<TextMeshProUGUI>();
+	[SerializeField] private List<TextMeshProUGUI> _selectedCharactersNameSimple = new List<TextMeshProUGUI>();
+	[SerializeField] private List<TextMeshProUGUI> _selectedCharactersNameDouble = new List<TextMeshProUGUI>();
+	private List<Transform> _currentCharacterModelLocation = new List<Transform>();
+	[SerializeField] private List<Transform> _characterModelLocationSimple = new List<Transform>();
+	[SerializeField] private List<Transform> _characterModelLocationDouble = new List<Transform>();
+	 private List<Image> _currentSelectedCharacterBackground = new List<Image>();
+	[SerializeField] private List<Image> _selectedCharacterBackgroundSimple = new List<Image>();
+	[SerializeField] private List<Image> _selectedCharacterBackgroundDouble = new List<Image>();
 	[SerializeField] private LayerMask _characterUILayerMask;
 	[SerializeField] private Transform _charactersModelsParent;
 	[SerializeField] private Button _playButton;
+	[SerializeField] private GameObject _playersShowRoomSimple;
+	[SerializeField] private GameObject _playersShowRoomDouble;
 	private Dictionary<string, GameObject> _charactersModel = new Dictionary<string, GameObject>();
-	private List<CharacterData> _playersCharacter = new List<CharacterData>(new CharacterData[4]);
-	private List<CharacterUI> _selectedCharacterUIs = new List<CharacterUI>(new CharacterUI[4]);
+	private List<CharacterData> _playersCharacter;
+	private List<CharacterUI> _selectedCharacterUIs;
+	private List<CharacterUI> _selectableCharacters = new List<CharacterUI>();
 	private int _playerIndex = 0;
+	private int _nbOfPlayers;
 
 	private void Start()
 	{
 		VerifyCharacters();
 		GameObject go;
+
 		foreach (var item in _characters)
 		{
 			go = Instantiate(_characterUIPrefab, _charactersListTransform);
 			go.GetComponent<CharacterUI>().SetVisual(item);
+			_selectableCharacters.Add(go.GetComponent<CharacterUI>());
 		}
 
-		foreach(var item in _characters)
+		foreach (var item in _characters)
 		{
 			go = Instantiate(item.Model3D, _charactersModelsParent);
 			go.name = item.Name;
@@ -44,24 +57,24 @@ public class CharacterSelectionMenu : MonoBehaviour
 
 	private void Update()
 	{
-		if(Input.GetMouseButtonDown(0))
+		if (Input.GetMouseButtonDown(0))
 		{
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 			GameObject go;
 
-			if (Physics.Raycast(ray, out hit, float.PositiveInfinity, _characterUILayerMask) 
+			if (Physics.Raycast(ray, out hit, float.PositiveInfinity, _characterUILayerMask)
 				&& hit.collider.TryGetComponent<CharacterUI>(out CharacterUI characterUI)
 				&& !characterUI.IsSelected)
 			{
 				characterUI.SetSelected(true);
-				_selectedCharactersName[_playerIndex].text = characterUI.Character.Name;
-				_selectedCharacterBackground[_playerIndex].color = characterUI.Character.CharacterColor;
+				_currentSelectedCharactersName[_playerIndex].text = characterUI.Character.Name;
+				_currentSelectedCharacterBackground[_playerIndex].color = characterUI.Character.CharacterColor;
 
-				if (_characterModelLocation[_playerIndex].childCount > 0)
+				if (_currentCharacterModelLocation[_playerIndex].childCount > 0)
 				{
 					_selectedCharacterUIs[_playerIndex].SetSelected(false);
-					go = _characterModelLocation[_playerIndex].GetChild(0).gameObject;
+					go = _currentCharacterModelLocation[_playerIndex].GetChild(0).gameObject;
 					go.transform.SetParent(_charactersListTransform);
 					go.transform.localPosition = Vector3.zero;
 					go.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
@@ -70,14 +83,14 @@ public class CharacterSelectionMenu : MonoBehaviour
 				}
 
 				_charactersModel.TryGetValue(characterUI.Character.Name, out go);
-				go.transform.SetParent(_characterModelLocation[_playerIndex]);
+				go.transform.SetParent(_currentCharacterModelLocation[_playerIndex]);
 				go.transform.localPosition = Vector3.zero;
 				go.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
 				go.SetActive(true);
 				_selectedCharacterUIs[_playerIndex] = characterUI;
 				_playersCharacter[_playerIndex] = characterUI.Character;
 				VerifyCharacters();
-				_playerIndex = (_playerIndex + 1) % 4;
+				_playerIndex = (_playerIndex + 1) % _nbOfPlayers;
 			}
 		}
 	}
@@ -90,12 +103,68 @@ public class CharacterSelectionMenu : MonoBehaviour
 
 	private void VerifyCharacters()
 	{
-		foreach(var item in _playersCharacter)
+		foreach (var item in _playersCharacter)
 		{
 			if (item == null)
 				return;
 		}
 
 		_playButton.interactable = true;
+	}
+
+	public void SetNumberOfShowrooms(bool isDouble)
+	{
+		if (isDouble)
+		{
+			_playersShowRoomDouble.SetActive(true);
+			_playersShowRoomSimple.SetActive(false);
+			_nbOfPlayers = 4;
+			_currentCharacterModelLocation = _characterModelLocationDouble;
+			_currentSelectedCharacterBackground = _selectedCharacterBackgroundDouble;
+			_currentSelectedCharactersName = _selectedCharactersNameDouble;
+		}
+		else
+		{
+			_playersShowRoomDouble.SetActive(false);
+			_playersShowRoomSimple.SetActive(true);
+			_nbOfPlayers = 2;
+			_currentCharacterModelLocation = _characterModelLocationSimple;
+			_currentSelectedCharacterBackground = _selectedCharacterBackgroundSimple;
+			_currentSelectedCharactersName = _selectedCharactersNameSimple;
+		}
+
+		_playersCharacter = new List<CharacterData>(new CharacterData[_nbOfPlayers]);
+		_selectedCharacterUIs = new List<CharacterUI>(new CharacterUI[_nbOfPlayers]);
+	}
+
+	public void ResetMenu()
+	{
+		foreach(var item in _charactersModel)
+		{
+			item.Value.transform.SetParent(_charactersListTransform);
+			item.Value.transform.localPosition = Vector3.zero;
+			item.Value.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+			item.Value.gameObject.SetActive(false);
+		}
+
+		foreach (var item in _currentSelectedCharacterBackground)
+		{
+			item.color = Color.black;
+		}
+
+		foreach (var item in _currentSelectedCharactersName)
+		{
+			item.text = "";
+		}
+
+		for (int i = 0; i < _playersCharacter.Count; i++)
+		{
+			_playersCharacter[i] = null;
+		}
+
+		foreach(var item in _selectableCharacters)
+		{
+			item.SetSelected(false);
+		}
 	}
 }
