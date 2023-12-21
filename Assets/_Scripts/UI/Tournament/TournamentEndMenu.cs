c4657b675ada343911b551f35ff5a2e14f20a678
boby <her.bastien@gmail.com>
2023-12-21 17:13:21 +0100

    Tournament End Menu finished
    Added particles for win and new background for win and lose. Also added Fadding text at bottom to continue

--- Assets/_Scripts/UI/Tournament/TournamentEndMenu.cs
+++ Assets/_Scripts/UI/Tournament/TournamentEndMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
+
 
 public class TournamentEndMenu : MonoBehaviour
 {
@@ -14,12 +16,21 @@ public class TournamentEndMenu : MonoBehaviour
     [SerializeField] private Transform _loserPlayerLocation;
 	[SerializeField] private GameObject _continueText;
     [SerializeField] private EventSystem _eventSystem;
+    [SerializeField] private TextMeshProUGUI _pressInputTextWin;
+    [SerializeField] private TextMeshProUGUI _pressInputTextLose;
+	[SerializeField] private TextMeshProUGUI _pressInputGlobal;
     private InputSystemUIInputModule _inputSystemUIInputModule;
 
-    private bool _canReturn = false;
-
-	public void SetWinnerMenu(GameObject winnerPrefab, Sprite cupSprite)
+    [SerializeField]private bool _canReturn = false;
+    private int _factor = -1;
+    private void Awake()
     {
+		_inputSystemUIInputModule = _eventSystem.gameObject.GetComponent<InputSystemUIInputModule>();
+    }
+
+    public void SetWinnerMenu(GameObject winnerPrefab, Sprite cupSprite)
+    {
+		_pressInputGlobal = _pressInputTextWin;
 		_continueText.SetActive(false);
 		_canReturn = false;
 		_winnerDisplay.SetActive(true);
@@ -38,7 +49,8 @@ public class TournamentEndMenu : MonoBehaviour
 
 	public void SetLoserMenu(GameObject loserPrefab)
 	{
-		_continueText.SetActive(false);
+		_pressInputGlobal = _pressInputTextLose;
+        _continueText.SetActive(false);
 		_canReturn = false;
 		_loserDisplay.SetActive(true);
 		_winnerDisplay.SetActive(false);
@@ -49,6 +61,7 @@ public class TournamentEndMenu : MonoBehaviour
 		go.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
 		StartCoroutine(WaitBeforeCanReturn());
     }
+
     private void Update()
     {
 		if(_inputSystemUIInputModule.submit.action.WasPressedThisFrame() && _canReturn)
@@ -64,15 +77,15 @@ public class TournamentEndMenu : MonoBehaviour
 			GameParameters.CurrentTournamentInfos.Reset();
 			MenuManager.Instance.GoBackToMainMenu();
 		}
-	}
-    private void Start()
-    {
-        _inputSystemUIInputModule = (InputSystemUIInputModule)_eventSystem.currentInputModule;
+        _pressInputGlobal.alpha = _pressInputGlobal.alpha + (1 * _factor * Time.deltaTime);
+
+        if (_pressInputGlobal.alpha < 0 || _pressInputGlobal.alpha > 1)
+            _factor *= -1;
     }
 
     private IEnumerator WaitBeforeCanReturn()
 	{
-		yield return new WaitForSeconds(3);
+		yield return new WaitForSeconds(3f);
 		_canReturn = true;
 		_continueText.SetActive(true);
 	}
