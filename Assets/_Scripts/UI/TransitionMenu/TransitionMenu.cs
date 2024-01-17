5c656b285ec6855ec0a23272ae212d07dd23c931
Remy <remy.jabbo@gmail.com>
2024-01-17 10:31:58 +0100

    End of Transition Menu

--- Assets/_Scripts/UI/TransitionMenu/TransitionMenu.cs
+++ Assets/_Scripts/UI/TransitionMenu/TransitionMenu.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TransitionMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _playerInfoPrefab;
     [SerializeField] private Transform _playerInfosContainer;
     [SerializeField] private Transform _playerLocationsParent;
+    [SerializeField] private List<Image> _playerTypeImages = new List<Image>(); 
+    [SerializeField] private List<TextMeshProUGUI> _playerTypeTexts = new List<TextMeshProUGUI>(); 
 
     private List<CharacterData> _playersCharacters;
     
@@ -22,6 +26,9 @@ public class TransitionMenu : MonoBehaviour
             go.GetComponent<PlayerInfos>().Init(_playersCharacters[i], playerType);
 		}
 
+        foreach (Transform child in _playerLocationsParent)
+            child.gameObject.SetActive(false);
+
         StartCoroutine(PlayersInstantiation());
 	}
 
@@ -31,8 +38,12 @@ public class TransitionMenu : MonoBehaviour
         {
 			yield return new WaitForSeconds(1f);
 
+            _playerLocationsParent.GetChild(i).gameObject.SetActive(true);
             MenuManager.Instance.PlaySound("CharacterAppearance");
 			GameObject go = Instantiate(_playersCharacters[i].BasicModel, _playerLocationsParent.GetChild(i));
+            _playerTypeImages[i].color = _playersCharacters[i].CharacterPrimaryColor;
+			string playerType = i >= GameParameters.Instance.LocalNbPlayers ? "COM" : $"P{i + 1}";
+            _playerTypeTexts[i].text = playerType;
             go.transform.localScale = new Vector3(10, 10, 10);
             go.transform.localPosition = Vector3.zero;
             go.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
